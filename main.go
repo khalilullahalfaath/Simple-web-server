48a7741e5fbdcad91e157b2b72a6c08ee19fa35c
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-08-18 09:31:59 +0700

    create routes

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "net/http"
 
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	// this simple web server can serve static files
+	// there are 3 routes: /, /form, /hello
+	// / is the root route, it serves static files
+	// /form is the route to the form page
+	// /hello is the route to the hello page
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
 }
